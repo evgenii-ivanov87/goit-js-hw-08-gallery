0e3d1ae36ff6e4add63818e3bbc4c3c62254ae6f
evgenii-ivanov87 <66361345+evgenii-ivanov87@users.noreply.github.com>
2020-11-26 21:05:57 +0200

    ор
    ор

---
+++ index.py
@@ -0,0 +1,15 @@
+class DataField:
+    field_description = "General"
+
+class FirstNameField(DataField):   
+    field_description = "Name"   
+
+class FirstCityField(DataField):    
+        field_description="City"
+class FirstSkillField(DataField):     
+        field_description="Skill"
+class FirstRateField(DataField):     
+        field_description="Rate"
+class FirstPhoneField(DataField):  
+        field_description = "Phone"
+        
